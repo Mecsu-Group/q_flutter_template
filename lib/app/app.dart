4675f50fff6d06b2c287b68dc87c8ce5e4db7917
Hung Q <htq287@gmail.com>
2023-12-28 17:16:56 +0700

    * add package 'flutter_screenutil' & config app in dynamic size

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:q_flutter_template/scenes/home/home_page.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../utils/app_extension.dart';
-
 import './app_route.dart';
 
 Future<void> bootstrap() async {
@@ -43,14 +42,66 @@ class _MyAppState extends ConsumerState<MyApp> {
   }
 
     // This widget is the root of your application.
-  @override
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Flutter Q.',
+  //     theme: ThemeData(
+  //       colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+  //       useMaterial3: true,
+  //     )
+  //   );
+  // }
+
+   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Q.',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-        useMaterial3: true,
-      )
+    /// Get providers
+    final AppRoute appRoute = AppRoute.I;
+
+    /// Init dynamic size
+    /// https://pub.dev/packages/flutter_screenutil
+    ///    ScreenUtil().pixelRatio       //Device pixel density
+    ///    ScreenUtil().screenWidth   (sdk>=2.6 : 1.sw)    //Device width
+    ///    ScreenUtil().screenHeight  (sdk>=2.6 : 1.sh)    //Device height
+    ///    ScreenUtil().bottomBarHeight  //Bottom safe zone distance, suitable for buttons with full screen
+    ///    ScreenUtil().statusBarHeight  //Status bar height , Notch will be higher Unit px
+    ///    ScreenUtil().textScaleFactor  //System font scaling factor
+    ///
+    ///    ScreenUtil().scaleWidth //Ratio of actual width dp to design draft px
+    ///    ScreenUtil().scaleHeight //Ratio of actual height dp to design draft px
+    ///
+    ///    0.2.sw  //0.2 times the screen width
+    ///    0.5.sh  //50% of screen height
+    /// Set the fit size (fill in the screen size of the device in the design)
+    /// https://www.paintcodeapp.com/news/ultimate-guide-to-iphone-resolutions
+    ///    iPhone 2, 3, 4, 4s                => 3.5": 320 x 480 (points)
+    ///    iPhone 5, 5s, 5C, SE (1st Gen)    => 4": 320 × 568 (points)
+    ///    iPhone 6, 6s, 7, 8, SE (2st Gen)  => 4.7": 375 x 667 (points)
+    ///    iPhone 6+, 6s+, 7+, 8+            => 5.5": 414 x 736 (points)
+    ///    iPhone 11Pro, X, Xs               => 5.8": 375 x 812 (points)
+    ///    iPhone 11, Xr                     => 6.1": 414 × 896 (points)
+    ///    iPhone 11Pro Max, Xs Max          => 6.5": 414 x 896 (points)
+    ///    iPhone 12 mini                    => 5.4": 375 x 812 (points)
+    ///    iPhone 12, 12 Pro                 => 6.1": 390 x 844 (points)
+    ///    iPhone 12 Pro Max                 => 6.7": 428 x 926 (points)
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (_, __) => MaterialApp(
+        navigatorKey: appRoute.navigatorKey,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+          useMaterial3: true,
+        ),
+        //https://stackoverflow.com/questions/57245175/flutter-dynamic-initial-route
+        //https://github.com/flutter/flutter/issues/12454
+        // home: (appRoute.generateRoute(const RouteSettings(name: AppRoute.routeRoot)) as MaterialPageRoute<dynamic>)
+        //     .builder(context),
+        initialRoute: AppRoute.routeSignIn,
+        onGenerateRoute: appRoute.generateRoute,
+        navigatorObservers: <NavigatorObserver>[appRoute.routeObserver],
+      ),
     );
   }
 }
